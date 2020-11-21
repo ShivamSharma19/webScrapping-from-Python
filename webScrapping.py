126fe1ff33f85a01278f9954dccb255fe0397b85
ShivamSharma19 <74807274+ShivamSharma19@users.noreply.github.com>
2020-11-21 11:56:04 +0530

    Create webScrapping.py

---
+++ webScrapping.py
@@ -0,0 +1,11 @@
+import requests
+import bs4
+res=requests.get('paste here link of any websites')
+res.test
+
+# If you what to take indivisual i of html then we code like this
+#here we make soup an object
+
+soup=bs4.BeautifulSoup(res.text,'lxml')
+hi=soup.select('title')
+print(hi)
