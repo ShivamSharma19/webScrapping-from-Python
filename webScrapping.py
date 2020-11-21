bc674bf27ac76367e14acba825c279dd9babd79d
ShivamSharma19 <74807274+ShivamSharma19@users.noreply.github.com>
2020-11-21 22:25:16 +0530

    Update webScrapping.py

--- webScrapping.py
+++ webScrapping.py
@@ -3,7 +3,7 @@ import bs4
 res=requests.get('paste here link of any websites')
 res.test
 
-# If you what to take indivisual i of html then we code like this
+# If you what to take indivisual parts of html then we code like this
 #here we make soup an object
 
 soup=bs4.BeautifulSoup(res.text,'lxml')
